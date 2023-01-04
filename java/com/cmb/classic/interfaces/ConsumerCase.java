914dbb734a3d492f37e132c15bf61b6a70809546
liujingmao <160143836@qq.com>
2023-01-04 19:21:25 +0800

    Method Ref and Constructor Ref

--- java/com/cmb/classic/interfaces/ConsumerCase.java
+++ java/com/cmb/classic/interfaces/ConsumerCase.java
@@ -1,5 +1,7 @@
 package com.cmb.classic.interfaces;
 
+import java.util.Comparator;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -47,14 +49,54 @@ public class ConsumerCase {
         s.forEach(con);
     }
 
+    public void testE(){
+        Consumer<String> c = (x)-> System.out.println(x);
+        c.accept("abc_lambda expression");
+
+        Consumer<String> c1 = System.out::println;
+        c1.accept("test_method_reference");
+    }
+
+    public void testF(){
+        Comparator<Integer> comp = (n, m)->Integer.compare(n,m);
+        int cop = comp.compare(3,4);
+        System.out.println(cop);
+
+        Comparator<Integer> comp1 = Integer::compare;
+        int com1 = comp1.compare(6,4);
+        System.out.println(com1);
+
+    }
+
+    public void testH(){
+        BiPredicate<String,String> b = (x,y)->x.equals(y);
+        System.out.println(b.test("bn","bn")); //true
+
+        BiPredicate<String,String> b1 = (a,c)->a.equals(c);
+        System.out.println(b1.test("test","test"));//true
+
+        BiPredicate<String,String> c1 = String::equals;
+        System.out.println(c1.test("c2","c3"));//false
+
+        BiPredicate<Integer,Integer> biPredicate = Integer::equals;
+        System.out.println(biPredicate.test(1,2));
+    }
+
 
 
     public static void main(String[] args) {
 
         ConsumerCase consumerCase = new ConsumerCase();
-        consumerCase.testC();
+        //consumerCase.testC();
 
-        consumerCase.testD();
+        //consumerCase.testD();
+
+        System.out.println("Parting Line");
+        //consumerCase.testE();
+
+       // consumerCase.testF();
+
+        consumerCase.testH();
 
 
 
